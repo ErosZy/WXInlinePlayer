734dfd8dd5e56fdc9461aca6478a8e44b568e117
吴剑明 <jianmingwu@kugou.net>
2021-06-24 22:20:05 +0800

    fix: 支持离屏渲染

--- src/drawer/drawer.js
+++ src/drawer/drawer.js
@@ -47,8 +47,10 @@ OTHERWISE, ARISING FROM, OUT OF OR IN ANY WAY CONNECTION WITH THE
 LICENSED WORK OR THE USE OR OTHER DEALINGS IN THE LICENSED WORK.
 *********************************************************/
 
-class Drawer {
-  constructor($canvas) {
+import Util from "../util/util";
+
+export function createDrawer(drawerType) {
+  function Drawer({ $canvas, isWorker }) {
     this.$canvas = $canvas;
     this.contextGL = null;
     this.shaderProgram = null;
@@ -56,158 +58,170 @@ class Drawer {
     this.yTextureRef = null;
     this.uTextureRef = null;
     this.vTextureRef = null;
-    this._initContextGL();
+    this.isWorkerRender = typeof OffscreenCanvas !== "undefined";
+    this.isWorker = isWorker;
 
-    if (this.contextGL) {
-      this._initProgram();
-      this._initBuffers();
-      this._initTextures();
+    if (this.isWorkerRender && !this.isWorker) {
+
+      // 支持离屏渲染，创建一个worker渲染
+      const BackgroundDrawer = Util.workerify(createDrawer, [
+        "drawNextOutputPicture",
+      ]);
+      this.bgDrawer = new BackgroundDrawer({ isWorker: true });
+      this.renderer = this.$canvas.getContext("bitmaprenderer");
+    } else if (this.$canvas) {
+      this.init();
     }
   }
 
-  static isSupport() {
-    const canvas = document.createElement('canvas');
-    const validContextNames = [
-      'webgl',
-      'experimental-webgl',
-      'moz-webgl',
-      'webkit-3d'
-    ];
+  Drawer.prototype = {
+    init() {
+      this._initContextGL();
 
-    let gl = null;
-    let nameIndex = 0;
-    while (!gl && nameIndex < validContextNames.length) {
-      var contextName = validContextNames[nameIndex];
+      if (this.contextGL) {
+        this._initProgram();
+        this._initBuffers();
+        this._initTextures();
+      }
+    },
+    drawNextOutputPicture({ width, height, data }) {
+      if (!this.isWorker && this.isWorkerRender) {
+
+        // 离屏渲染模式，将数据发送给worker线程
+        return this.bgDrawer
+          .drawNextOutputPicture({ width, height, data })
+          .then((data) => {
+            this.renderer.transferFromImageBitmap(data);
+          });
+      } else if (!this.$canvas && this.isWorker) {
+        this.$canvas = new OffscreenCanvas(width, height);
+        this.init();
+      }
+
+      const { yTextureRef, uTextureRef, vTextureRef } = this;
+      const gl = this.contextGL;
+      gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+      gl.viewport(0, 0, width, height);
+
+      const i420Data = data;
+      const yDataLength = width * height;
+      const yData = i420Data.subarray(0, yDataLength);
+      gl.activeTexture(gl.TEXTURE0);
+      gl.bindTexture(gl.TEXTURE_2D, yTextureRef);
+      gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        gl.LUMINANCE,
+        width,
+        height,
+        0,
+        gl.LUMINANCE,
+        gl.UNSIGNED_BYTE,
+        yData
+      );
+
+      const cbDataLength = (width * height) / 4;
+      const cbData = i420Data.subarray(yDataLength, yDataLength + cbDataLength);
+      gl.activeTexture(gl.TEXTURE1);
+      gl.bindTexture(gl.TEXTURE_2D, uTextureRef);
+      gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        gl.LUMINANCE,
+        width / 2,
+        height / 2,
+        0,
+        gl.LUMINANCE,
+        gl.UNSIGNED_BYTE,
+        cbData
+      );
+
+      const crDataLength = cbDataLength;
+      const crData = i420Data.subarray(
+        yDataLength + cbDataLength,
+        yDataLength + cbDataLength + crDataLength
+      );
+
+      gl.activeTexture(gl.TEXTURE2);
+      gl.bindTexture(gl.TEXTURE_2D, vTextureRef);
+      gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        gl.LUMINANCE,
+        width / 2,
+        height / 2,
+        0,
+        gl.LUMINANCE,
+        gl.UNSIGNED_BYTE,
+        crData
+      );
+
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+      // 离屏渲染模式，worker线程，返回数据
+      if (this.isWorker) {
+        return this.$canvas.transferToImageBitmap();
+      }
+    },
+
+    destroy() {
       try {
-        gl = canvas.getContext(contextName, { preserveDrawingBuffer: true });
-      } catch (e) {
-        gl = null;
+        this.contextGL.getExtension("WEBGL_lose_context").loseContext();
+      } catch (e) {}
+
+      this.$canvas = null;
+      this.contextGL = null;
+      this.shaderProgram = null;
+      this.texturePosBuffer = null;
+      this.yTextureRef = null;
+      this.uTextureRef = null;
+      this.vTextureRef = null;
+
+      if (this.bgDrawer) {
+        this.bgDrawer.terminate();
       }
-      if (!gl || typeof gl.getParameter !== 'function') {
-        gl = null;
+    },
+
+    _initContextGL() {
+      const canvas = this.$canvas;
+      const validContextNames = [
+        "webgl",
+        "experimental-webgl",
+        "moz-webgl",
+        "webkit-3d",
+      ];
+
+      let gl = null;
+      let nameIndex = 0;
+      while (!gl && nameIndex < validContextNames.length) {
+        var contextName = validContextNames[nameIndex];
+        try {
+          gl = canvas.getContext(contextName);
+        } catch (e) {
+          gl = null;
+        }
+        if (!gl || typeof gl.getParameter !== "function") {
+          gl = null;
+        }
+        ++nameIndex;
       }
-      ++nameIndex;
-    }
 
-    return !!gl;
-  }
+      this.contextGL = gl;
+    },
 
-  drawNextOutputPicture(width, height, data) {
-    const { yTextureRef, uTextureRef, vTextureRef } = this;
-    const gl = this.contextGL;
-    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
-    gl.viewport(0, 0, width, height);
-
-    const i420Data = data;
-    const yDataLength = width * height;
-    const yData = i420Data.subarray(0, yDataLength);
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, yTextureRef);
-    gl.texImage2D(
-      gl.TEXTURE_2D,
-      0,
-      gl.LUMINANCE,
-      width,
-      height,
-      0,
-      gl.LUMINANCE,
-      gl.UNSIGNED_BYTE,
-      yData
-    );
-
-    const cbDataLength = width * height / 4;
-    const cbData = i420Data.subarray(yDataLength, yDataLength + cbDataLength);
-    gl.activeTexture(gl.TEXTURE1);
-    gl.bindTexture(gl.TEXTURE_2D, uTextureRef);
-    gl.texImage2D(
-      gl.TEXTURE_2D,
-      0,
-      gl.LUMINANCE,
-      width / 2,
-      height / 2,
-      0,
-      gl.LUMINANCE,
-      gl.UNSIGNED_BYTE,
-      cbData
-    );
-
-    const crDataLength = cbDataLength;
-    const crData = i420Data.subarray(
-      yDataLength + cbDataLength,
-      yDataLength + cbDataLength + crDataLength
-    );
-
-    gl.activeTexture(gl.TEXTURE2);
-    gl.bindTexture(gl.TEXTURE_2D, vTextureRef);
-    gl.texImage2D(
-      gl.TEXTURE_2D,
-      0,
-      gl.LUMINANCE,
-      width / 2,
-      height / 2,
-      0,
-      gl.LUMINANCE,
-      gl.UNSIGNED_BYTE,
-      crData
-    );
-
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  }
-
-  destroy() {
-    try {
-      this.contextGL.getExtension('WEBGL_lose_context').loseContext();
-    } catch (e) {}
-
-    this.$canvas = null;
-    this.contextGL = null;
-    this.shaderProgram = null;
-    this.texturePosBuffer = null;
-    this.yTextureRef = null;
-    this.uTextureRef = null;
-    this.vTextureRef = null;
-  }
-
-  _initContextGL() {
-    const canvas = this.$canvas;
-    const validContextNames = [
-      'webgl',
-      'experimental-webgl',
-      'moz-webgl',
-      'webkit-3d'
-    ];
-
-    let gl = null;
-    let nameIndex = 0;
-    while (!gl && nameIndex < validContextNames.length) {
-      var contextName = validContextNames[nameIndex];
-      try {
-        gl = canvas.getContext(contextName);
-      } catch (e) {
-        gl = null;
-      }
-      if (!gl || typeof gl.getParameter !== 'function') {
-        gl = null;
-      }
-      ++nameIndex;
-    }
-
-    this.contextGL = gl;
-  }
-
-  _initProgram() {
-    const gl = this.contextGL;
-    const vertexShaderScript = `
+    _initProgram() {
+      const gl = this.contextGL;
+      const vertexShaderScript = `
     attribute vec4 vertexPos;
     attribute vec4 texturePos;
     varying vec2 textureCoord;
     void main(){
-        gl_Position = vertexPos; 
+        gl_Position = vertexPos;
         textureCoord = texturePos.xy;
     }
     `;
 
-    const fragmentShaderScript = `
+      const fragmentShaderScript = `
     precision highp float;
     varying highp vec2 textureCoord;
     uniform sampler2D ySampler;
@@ -228,101 +242,150 @@ class Drawer {
     }
     `;
 
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vertexShaderScript);
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-      console.log(
-        'Vertex shader failed to compile: ' + gl.getShaderInfoLog(vertexShader)
+      const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+      gl.shaderSource(vertexShader, vertexShaderScript);
+      gl.compileShader(vertexShader);
+      if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        console.log(
+          "Vertex shader failed to compile: " +
+            gl.getShaderInfoLog(vertexShader)
+        );
+      }
+
+      const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+      gl.shaderSource(fragmentShader, fragmentShaderScript);
+      gl.compileShader(fragmentShader);
+      if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        console.log(
+          "Fragment shader failed to compile: " +
+            gl.getShaderInfoLog(fragmentShader)
+        );
+      }
+
+      const program = gl.createProgram();
+      gl.attachShader(program, vertexShader);
+      gl.attachShader(program, fragmentShader);
+      gl.linkProgram(program);
+      if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.log(
+          "Program failed to compile: " + gl.getProgramInfoLog(program)
+        );
+      }
+
+      gl.useProgram(program);
+      this.shaderProgram = program;
+    },
+
+    _initBuffers() {
+      const gl = this.contextGL;
+      const program = this.shaderProgram;
+      const vertexPosBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]),
+        gl.STATIC_DRAW
       );
+
+      const vertexPosRef = gl.getAttribLocation(program, "vertexPos");
+      gl.enableVertexAttribArray(vertexPosRef);
+      gl.vertexAttribPointer(vertexPosRef, 2, gl.FLOAT, false, 0, 0);
+
+      const texturePosBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, texturePosBuffer);
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([1, 0, 0, 0, 1, 1, 0, 1]),
+        gl.STATIC_DRAW
+      );
+
+      var texturePosRef = gl.getAttribLocation(program, "texturePos");
+      gl.enableVertexAttribArray(texturePosRef);
+      gl.vertexAttribPointer(texturePosRef, 2, gl.FLOAT, false, 0, 0);
+
+      this.texturePosBuffer = texturePosBuffer;
+    },
+
+    _initTextures() {
+      const gl = this.contextGL;
+      const program = this.shaderProgram;
+
+      const yTextureRef = this._initTexture();
+      const ySamplerRef = gl.getUniformLocation(program, "ySampler");
+      gl.uniform1i(ySamplerRef, 0);
+      this.yTextureRef = yTextureRef;
+
+      const uTextureRef = this._initTexture();
+      const uSamplerRef = gl.getUniformLocation(program, "uSampler");
+      gl.uniform1i(uSamplerRef, 1);
+      this.uTextureRef = uTextureRef;
+
+      const vTextureRef = this._initTexture();
+      const vSamplerRef = gl.getUniformLocation(program, "vSampler");
+      gl.uniform1i(vSamplerRef, 2);
+      this.vTextureRef = vTextureRef;
+    },
+
+    _initTexture() {
+      const gl = this.contextGL;
+      const textureRef = gl.createTexture();
+      gl.bindTexture(gl.TEXTURE_2D, textureRef);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      gl.bindTexture(gl.TEXTURE_2D, null);
+
+      return textureRef;
+    },
+  };
+
+  Drawer.isSupport = () => {
+    const canvas = document.createElement("canvas");
+    const validContextNames = [
+      "webgl",
+      "experimental-webgl",
+      "moz-webgl",
+      "webkit-3d",
+    ];
+
+    let gl = null;
+    let nameIndex = 0;
+    while (!gl && nameIndex < validContextNames.length) {
+      var contextName = validContextNames[nameIndex];
+      try {
+        gl = canvas.getContext(contextName, { preserveDrawingBuffer: true });
+      } catch (e) {
+        gl = null;
+      }
+      if (!gl || typeof gl.getParameter !== "function") {
+        gl = null;
+      }
+      ++nameIndex;
     }
 
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragmentShaderScript);
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-      console.log(
-        'Fragment shader failed to compile: ' +
-          gl.getShaderInfoLog(fragmentShader)
-      );
-    }
+    return !!gl;
+  };
 
-    const program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-      console.log(
-        'Program failed to compile: ' + gl.getProgramInfoLog(program)
-      );
-    }
-
-    gl.useProgram(program);
-    this.shaderProgram = program;
+  // 支持离屏渲染时，收听线程信息
+  if (typeof drawerType === "undefined") {
+    let drawer = null;
+    self.onmessage = ({ data: { id, type, data } }) => {
+      switch (type) {
+        case "constructor":
+          drawer = new Drawer(data);
+          break;
+        case "drawNextOutputPicture":
+          if (drawer) {
+            const bitmap = drawer.drawNextOutputPicture(data);
+            self.postMessage({ id: id, data: bitmap }, [bitmap]);
+          }
+          break;
+      }
+    };
   }
 
-  _initBuffers() {
-    const gl = this.contextGL;
-    const program = this.shaderProgram;
-    const vertexPosBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
-    gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]),
-      gl.STATIC_DRAW
-    );
-
-    const vertexPosRef = gl.getAttribLocation(program, 'vertexPos');
-    gl.enableVertexAttribArray(vertexPosRef);
-    gl.vertexAttribPointer(vertexPosRef, 2, gl.FLOAT, false, 0, 0);
-
-    const texturePosBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, texturePosBuffer);
-    gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array([1, 0, 0, 0, 1, 1, 0, 1]),
-      gl.STATIC_DRAW
-    );
-
-    var texturePosRef = gl.getAttribLocation(program, 'texturePos');
-    gl.enableVertexAttribArray(texturePosRef);
-    gl.vertexAttribPointer(texturePosRef, 2, gl.FLOAT, false, 0, 0);
-
-    this.texturePosBuffer = texturePosBuffer;
-  }
-
-  _initTextures() {
-    const gl = this.contextGL;
-    const program = this.shaderProgram;
-
-    const yTextureRef = this._initTexture();
-    const ySamplerRef = gl.getUniformLocation(program, 'ySampler');
-    gl.uniform1i(ySamplerRef, 0);
-    this.yTextureRef = yTextureRef;
-
-    const uTextureRef = this._initTexture();
-    const uSamplerRef = gl.getUniformLocation(program, 'uSampler');
-    gl.uniform1i(uSamplerRef, 1);
-    this.uTextureRef = uTextureRef;
-
-    const vTextureRef = this._initTexture();
-    const vSamplerRef = gl.getUniformLocation(program, 'vSampler');
-    gl.uniform1i(vSamplerRef, 2);
-    this.vTextureRef = vTextureRef;
-  }
-
-  _initTexture() {
-    const gl = this.contextGL;
-    const textureRef = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, textureRef);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.bindTexture(gl.TEXTURE_2D, null);
-
-    return textureRef;
-  }
+  return Drawer;
 }
 
-export default Drawer;
+export default createDrawer(0);
